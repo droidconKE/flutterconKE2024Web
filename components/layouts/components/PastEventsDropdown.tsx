b702d1da824e0f17f87fcfae5d4a9d7722626e18
Magak Emmanuel <emashmagak@gmail.com>
2025-04-04 21:37:59 +0300

    added content details

--- components/layouts/components/PastEventsDropdown.tsx
+++ components/layouts/components/PastEventsDropdown.tsx
@@ -72,12 +72,12 @@ export const PastEventsDropdown = () => {
         >
           <div className="py-1 text-sm" role="none">
             <Link
-              href="/past-events/2023"
+              href="/past-events/2024"
               className={
-                router.pathname === '/past-events/2023' ? 'active-link' : 'link'
+                router.pathname === '/past-events/2024' ? 'active-link' : 'link'
               }
             >
-              flutterconKe 2023
+              flutterconKe 2024
             </Link>
           </div>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -29,13 +29,10 @@ const Home: NextPage<HomeProps> = ({ organizers, sponsors }) => {
               <span className="font-medium">flutterconke</span>
             </h2>
             <p className="dark:text-lighter-dark py-5 md:py-8 text-xl md:text-2xl">
-              Fluttercon Kenya stands as the pioneering event of its kind in
-              Africa, marking the debut of the Fluttercon conference on the
-              continent. Designed to be a hub of innovation and learning,
-              Fluttercon Kenya will offer a rich program of tech talks,
-              workshops, and panels led by industry experts, Google Developer
-              Experts, and seasoned Flutter specialists, all dedicated to
-              exploring the latest in Flutter and Dart technologies.
+              Fluttercon is a global conference focused on the engineering of
+              Mobile applications. Fluttercon provides a forum for developers to
+              network with other developers, share techniques, announce apps and
+              products, and learn and teach.
             </p>
             <Link href="/about" className="lowercase text-xl font-bold">
               more about flutterconke
